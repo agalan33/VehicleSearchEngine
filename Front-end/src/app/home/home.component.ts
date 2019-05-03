b283ebd8f3534e6be5f07853639e648d5345962b
adahidgalan <adahid.galan@upr.edu>
2019-05-03 11:10:57 -0400

    Working in Phase2 Added all dealers, dealer details, vehicles in dealer, etc. added login and profile in initial states Added material services and domain layer Added vehicle details

--- Front-end/src/app/home/home.component.ts
+++ Front-end/src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Employee } from '../../domain/employee.type';
+import {Vehicle} from '../../domain/vehicle.type';
 
 @Component({
   selector: 'app-home',
@@ -11,13 +12,28 @@ export class HomeComponent implements OnInit {
 
   constructor(private httpClient: HttpClient) { }
   private employeeURL = 'http://127.0.0.1:5000/VehicleSearchEngine/Employees/1';
+  private vehiclesURL = 'http://127.0.0.1:5000/VehicleSearchEngine/vehicles';
   private employee: Employee;
+  private allVehicles = [];
+  private topVehicles: Vehicle[] = [];
+  headers = ['position', 'vbrand', 'vmodel', 'vyear', 'vcolor', 'vprice'];
+
   ngOnInit() {
     this.getEmployeeInformation();
+    this.getTopVehicles();
   }
   getEmployeeInformation() {
     this.httpClient.get<Employee>(this.employeeURL).subscribe(data => {
       this.employee = data;
+    });
+  }
+
+  getTopVehicles() {
+    this.httpClient.get<Vehicle[]>(this.vehiclesURL).subscribe(data => {
+      this.allVehicles = data;
+      for (let i = 0; i < 10; i++) {
+        this.topVehicles.push(this.allVehicles[Math.floor(Math.random() * this.allVehicles.length)]);
+      }
     });
   }
 
